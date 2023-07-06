ad817ad97fcb3061dc48d6e1bb0eacd3eed84e36
Lakshay Malhotra <lakshay.sbp@gmail.com>
2023-07-06 18:49:19 +0530

    recovered..

--- utils/index.ts
+++ utils/index.ts
@@ -1,31 +1,14 @@
-// export async function fetchCars() {
-// const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': 'ee0d6da747msh402625585fcb6f7p17cba5jsn43cb1bebf151',
-// 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-// }
 
 export async function fetchCars() {
     const headers = {
-        headers: {
+        
             'X-RapidAPI-Key': 'ee0d6da747msh402625585fcb6f7p17cba5jsn43cb1bebf151',
             'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
         }
-    }
-    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',  {
-        // headers: headers,
+    
+    const response = await fetch ('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', 
+     {
+        headers: headers,
     }
     );
         const result = await response.json();
